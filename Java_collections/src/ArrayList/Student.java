3b68a0a02e6cb32940b8f35eb8fd133e56d1641b
Chandrashekar <nagasantoshchavvakula@gmail.com>
2023-12-22 15:07:31 -0600

    User-defined class objects in Java ArrayList

---
+++ Java_collections/src/ArrayList/Student.java
@@ -0,0 +1,14 @@
+package ArrayList;
+
+public class Student extends ArrayList5  {
+
+	int rollno;
+	String name;
+	int age;
+	Student(int rollno,String name,int age){
+		this.rollno=rollno;
+		this.name=name;
+		this.age=age;
+	}
+
+}
