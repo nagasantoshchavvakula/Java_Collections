a4e3ccdc01b644963cdd9253873a204b5728f399
Chandrashekar <nagasantoshchavvakula@gmail.com>
2023-12-23 12:47:01 -0600

    Example_Book

--- Java_collections/src/ArrayList/Student.java
+++ Java_collections/src/ArrayList/Student.java
@@ -5,7 +5,7 @@ public class Student extends ArrayList5  {
 	int rollno;
 	String name;
 	int age;
-	Student(int rollno,String name,int age){
+	public Student(int rollno,String name,int age){
 		this.rollno=rollno;
 		this.name=name;
 		this.age=age;
