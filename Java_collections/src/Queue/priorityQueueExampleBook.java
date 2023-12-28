b9116525645f4fc1f31ddcf6e2e1d52ddb22b094
Chandrashekar <nagasantoshchavvakula@gmail.com>
2023-12-28 14:48:21 -0600

    Queue

---
+++ Java_collections/src/Queue/priorityQueueExampleBook.java
@@ -0,0 +1,60 @@
+//PriorityQueue example where we are adding books to queue and printing all the books
+package Queue;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+//import ArrayList.Book;
+class Book implements Comparable<Book>{
+	int id;
+	String name,author,publisher;
+	int quantity;
+	public Book(int id, String name, String author, String publisher, int quantity) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.author = author;
+		this.publisher = publisher;
+		this.quantity = quantity;
+	}
+	@Override
+	public int compareTo(Book b) {
+		// TODO Auto-generated method stub
+		if(id>b.id) {
+			return 1;
+		}else if(id<b.id) {
+			return -1;
+		}else {
+			return 0;
+		}
+		
+	}
+	
+}
+
+public class priorityQueueExampleBook {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PriorityQueue<Book> queue=new PriorityQueue<Book>();  
+		    //Creating Books  
+		    Book b1=new Book(121,"Let us C","Yashwant Kanetkar","BPB",8);  
+		    Book b2=new Book(233,"Operating System","Galvin","Wiley",6);  
+		    Book b3=new Book(101,"Data Communications & Networking","Forouzan","Mc Graw Hill",4);  
+		    //Adding Books to the queue  
+		    queue.add(b1);  
+		    queue.add(b2);  
+		    queue.add(b3);  
+		    System.out.println("Traversing the queue elements:");  
+		    //Traversing queue elements  
+		    for(Book b:queue){  
+		    System.out.println(b.id+" "+b.name+" "+b.author+" "+b.publisher+" "+b.quantity);  
+		    }  
+		    queue.remove();  
+		    System.out.println("After removing one book record:");  
+		    for(Book b:queue){  
+		        System.out.println(b.id+" "+b.name+" "+b.author+" "+b.publisher+" "+b.quantity);  
+		        }  
+	}
+
+}
