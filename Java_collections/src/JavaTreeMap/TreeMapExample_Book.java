c2b718203d5aa5fdaa1cc43e6236c5371cdc3142
Chandrashekar <nagasantoshchavvakula@gmail.com>
2024-01-18 13:37:45 -0600

    JavaTreeMap

---
+++ Java_collections/src/JavaTreeMap/TreeMapExample_Book.java
@@ -0,0 +1,50 @@
+package JavaTreeMap;
+
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
+//import ArrayList.Book;
+
+class Book{
+	int id;
+	String name,author,publisher;
+	int quantity;
+	public Book(int id, String name, String author, String publisher, int quantity) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.author = author;
+		this.publisher = publisher;
+		this.quantity = quantity;
+	}
+	
+}
+public class TreeMapExample_Book {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Creating Map for Books
+		TreeMap<Integer,Book>list=new TreeMap<Integer,Book>();
+		
+		//Creating Books
+		Book b1=new Book(101,"Let us C","Yashwant Kanetkar","BPB",8);    
+	    Book b2=new Book(102,"Data Communications & Networking","Forouzan","Mc Graw Hill",4);    
+	    Book b3=new Book(103,"Operating System","Galvin","Wiley",6);  
+	    
+		//Adding books
+		list.put(2,b2);
+		list.put(1,b1);
+		list.put(3,b3);
+		
+		//Traversing 
+		for(Entry<Integer, Book> entry:list.entrySet()) {
+			int key=entry.getKey();
+			Book b=entry.getValue();
+			System.out.println(key+"Details");
+			System.out.println(b.id+" "+b.name+" "+b.author+" "+b.publisher+" "+b.quantity);
+		}
+		
+	}
+
+}
